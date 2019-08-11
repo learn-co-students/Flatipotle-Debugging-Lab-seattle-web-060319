b5f73f82229aa70075767b25fe0885db7048d723
Jason Leach <jlspursfan@gmail.com>
2019-08-11 13:44:45 -0700

    Done.

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react'
+import Side from './Side'
 
 class Order extends Component {
-  state = {
+
+    state = {
     isClicked: false
-  }
+    }
+  
 
   handleClick = () => {
-    this.state.isClicked = !this.state.isClicked
+    this.setState({
+      isClicked: !this.isClicked
+    })
   }
 
   render() {
